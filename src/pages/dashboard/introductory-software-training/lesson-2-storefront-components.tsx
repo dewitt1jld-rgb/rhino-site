95861872b46de91e9af9fe3a6ff9de357b3da1ee
Landon Dewitt <YOUR_GITHUB_EMAIL>
2026-08-06 12:28:22 -0700

    push again

--- src/pages/dashboard/introductory-software-training/lesson-2-storefront-components.tsx
+++ src/pages/dashboard/introductory-software-training/lesson-2-storefront-components.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import AcademyLessonLayout from "./AcademyLessonLayout";
+import AcademyLessonLayout from "../../../components/AcademyLessonLayout";
 
 const steps = [
   {
